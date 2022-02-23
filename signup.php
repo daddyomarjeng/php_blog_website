db173507f3a4a097d5a748e68f1cd300c988c505
DOJ <omarjeng999@gmail.com>
2022-02-23 01:50:57 +0000

    signup page UI

--- signup.php
+++ signup.php
@@ -1,46 +1,26 @@
-<?php 
+<?php
 include('includes/header.php');
 
 ?>
 
-<div class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-5">
-                <div class="card">
-
-                    <div class="card-header">
-                        <h4>Sign Up</h4>
-                    </div>
-                    <div class="card-body">
-                        <form action="code.php" method="post">
-                            <div class="form-group mb-3">
-                                <label for="fname">First Name</label>
-                                <input class="form-control" type="text" name="fname" id="fname" placeholder="Enter First Name">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="lname">Last Name</label>
-                                <input class="form-control" type="text" name="lname" id="lname" placeholder="Enter Last Name">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="email">Email</label>
-                                <input class="form-control" type="text" name="email" id="email" placeholder="Enter Email">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="password">Password</label>
-                                <input class="form-control" type="password" name="password" id="password" placeholder="Enter Password">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="confirm_password">Confirm Password</label>
-                                <input class="form-control" type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Your Password">
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
+<div class="form-container">
+    <h1 class="form-header">SignUp Form</h1>
+    <form action="func.php" method="post">
+        <div class="form-group">
+            <label for="fullname">Fullname</label>
+            <input type="text" class="form-control" placeholder="Fullname">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="fullname">Email</label>
+            <input type="email" class="form-control" placeholder="Fullname">
+        </div>
+        <div class="form-group">
+            <label for="fullname">Password</label>
+            <input type="password" class="form-control" placeholder="Fullname">
+        </div>
+        <button type="submit" class="btn btn-submit">Login</button>
+    </form>
 </div>
+
 
 <?php include('includes/footer.php') ?>
