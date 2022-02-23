ceabf3e71d4ed09e19f122cc85a001e3f4d668bf
DOJ <omarjeng999@gmail.com>
2022-02-23 01:48:39 +0000

    login page UI

--- login.php
+++ login.php
@@ -1,34 +1,26 @@
-<?php 
+<?php
 include('includes/header.php');
 
 ?>
 
-<div class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-5">
-                <div class="card">
-
-                    <div class="card-header">
-                        <h4>Login</h4>
-                    </div>
-                    <div class="card-body">
-                        <form action="code.php" method="post">
-                            <div class="form-group mb-3">
-                                <label for="email">Email</label>
-                                <input class="form-control" type="text" name="email" id="email" placeholder="Enter Email">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="password">Password</label>
-                                <input class="form-control" type="password" name="password" id="password" placeholder="Enter Password">
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
+<div class="form-container">
+    <h1 class="form-header">Login Form</h1>
+    <form action="func.php" method="post">
+        <div class="form-group">
+            <label for="fullname">Fullname</label>
+            <input type="text" class="form-control" placeholder="Fullname">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="fullname">Email</label>
+            <input type="email" class="form-control" placeholder="Fullname">
+        </div>
+        <div class="form-group">
+            <label for="fullname">Password</label>
+            <input type="password" class="form-control" placeholder="Fullname">
+        </div>
+        <button type="submit" class="btn btn-submit">Login</button>
+    </form>
 </div>
+
 
 <?php include('includes/footer.php') ?>
