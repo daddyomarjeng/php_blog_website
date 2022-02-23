e47f9f464b0d8dca79295fd50b24b5584a4d8654
DOJ <omarjeng999@gmail.com>
2022-02-23 00:49:21 +0000

    add navbar

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <a href="index.php" class="logo">
+        <img src="assets/images/logo.png" alt="Logo" height="50px">
+    </a>
+    <ul class="nav-items">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">PHP</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Javascript</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Java</a></li>
+    </ul>
+</nav>
