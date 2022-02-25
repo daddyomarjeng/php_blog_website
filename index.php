0ee25bbf56ddace693d4c3a70945811e8f85f6b7
DOJ <omarjeng999@gmail.com>
2022-02-25 22:00:24 +0000

    add banner and home layout

--- index.php
+++ index.php
@@ -4,10 +4,22 @@ include('includes/header.php');
 
 <div class="container">
 
-    <?php include('messages.php') ?>
+    <div class="banner">
+        <div class="overlay">
+            <span class="h-1">Are you a Writer?</span>
+            <span>Or you want to Start Writing?</span>
+            <a href="add-post.php" class="btn-home">Write Now</a>
+        </div>
+    </div>
 
-    <h1>Home Page</h1>
-
+    <section class="blogs-section">
+        <div class="blogs">
+            blogs
+        </div>
+        <div class="latest">
+            latest
+        </div>
+    </section>
 </div>
 
 <?php
